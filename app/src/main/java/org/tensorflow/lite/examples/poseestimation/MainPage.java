bf50d88504d4c61895c32d86b844b22ca6c741a4
Joanne <seven4asecret19@gmail.com>
2022-06-29 15:25:24 +0800

    add MainPage activity

---
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/MainPage.java
@@ -0,0 +1,53 @@
+package org.tensorflow.lite.examples.poseestimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainPage extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnMotionList,btnHistory,btnTrainingMenu,btnSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        btnMotionList = findViewById(R.id.btnMotionList);
+        btnHistory = findViewById(R.id.btnHistory);
+        btnTrainingMenu =findViewById(R.id.btnTrainingMenu);
+        btnSettings = findViewById(R.id.btnSettings);
+
+        btnMotionList.setOnClickListener(this);
+        btnHistory.setOnClickListener(this);
+        btnTrainingMenu.setOnClickListener(this);
+        btnSettings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        switch (v.getId()) {
+            case R.id.btnMotionList:
+                i = new Intent(this,MotionList.class);
+                break;
+            case R.id.btnHistory:
+                //i = new Intent(this,History.class);
+                break;
+            case R.id.btnTrainingMenu:
+                //i = new Intent(this,TrainMenu.class);
+                break;
+            case R.id.btnSettings:
+                i = new Intent(this,MainActivity.class);
+                break;
+            default:
+                break;
+        }
+        startActivity(i);
+    }
+}
