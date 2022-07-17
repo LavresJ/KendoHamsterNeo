dfbabd34e02bc67c4a754f466b9e7fbd3292bcea
Yang Yun Yeh <lim299792458ms@g.ncu.edu.tw>
2022-07-17 18:17:47 +0800

    add classifier and python

---
+++ app/src/main/java/com/kendohamster/SK.java
@@ -0,0 +1,17 @@
+package com.kendohamster;
+
+public class SK {
+    double sk[] = new double[36];
+
+    public SK(double[] sk) {
+        this.sk = sk;
+    }
+
+    public double[] getSk() {
+        return sk;
+    }
+
+    public void setSk(double[] sk) {
+        this.sk = sk;
+    }
+}
