32e3842ab3ed3089b1a77af7100799c4e9a9deed
gavinfu2 <gavinfu2@gmail.com>
2022-09-05 11:19:20 +0800

    Setting

--- app/src/main/java/com/kendohamster/Settings.java
+++ app/src/main/java/com/kendohamster/Settings.java
@@ -1,14 +1,22 @@
 package com.kendohamster;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.kendohamster.R;
 
 public class Settings extends AppCompatActivity {
@@ -42,7 +50,7 @@ public class Settings extends AppCompatActivity {
         listView2 = findViewById(R.id.listGeneral);
         settingsGeneral = getResources().getStringArray(R.array.settingsGeneral);
 
-        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, PersonalInformation);
+        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, settingsGeneral);
         listView2.setAdapter(adapter2);
 
         listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -52,5 +60,83 @@ public class Settings extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"選擇" + settingsGeneral,Toast.LENGTH_LONG).show();
             }
         });
+//        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerLayout);
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.drawer_open , R.string.drawer_close){
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                super .onDrawerClosed(drawerView);
+//            }
+//
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                super .onDrawerOpened(drawerView);
+//            }
+//        };
+//
+//        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                drawerLayout.closeDrawer(GravityCompat.START);
+//
+//                int id = item.getItemId();
+//                if (id == R.id.action_action){
+//                    selectItem(R.id.action_action);
+//
+//                    item.setChecked(true);
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
+//                else if (id == R.id.action_menu){
+//                    selectItem(R.id.action_menu);
+//
+//                    item.setChecked(true);
+//                    drawerLayout.closeDrawers();
+//
+//                    return true;
+//                }
+//                else if (id == R.id.action_history){
+//                    selectItem(R.id.action_history);
+//
+//                    item.setChecked(true);
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
+//                else if (id == R.id.action_setting){
+//                    selectItem(R.id.action_setting);
+//
+//                    item.setChecked(true);
+//                    drawerLayout.closeDrawers();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+    }
+    public void selectItem(int position) {
+        Intent i = null;
+        switch(position) {
+            case (R.id.action_action):
+                i = new Intent(this,MotionList.class);
+                break;
+            case R.id.action_history:
+                i = new Intent(this, History.class);
+                break;
+            case R.id.action_menu:
+                i = new Intent(this, TrainingMenu.class);
+                break;
+            case R.id.action_setting:
+                i = new Intent(this, Settings.class);
+                break;
+            default:
+                break;
+        }
+
+        startActivity(i);
     }
 }
